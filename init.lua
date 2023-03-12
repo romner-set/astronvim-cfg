cc891d4f6ad9379046b9a7ef764ccb37e152bbbd
romner-set <roman@skotnica.com>
2023-03-12 21:53:44 +0100

    stdnkdtnkssdtdtsrtdstdsrnekkne

--- init.lua
+++ init.lua
@@ -5,20 +5,29 @@ return {
     },
   },
   mappings = {
-    n = {j='k', k='j'}
+    n = { j = "k", k = "j" },
+  },
+  highlights = {
+    init = {
+      Normal = { bg = "NONE" },
+    },
   },
   updater = {
     channel = "nightly",
   },
   plugins = {
-    ["neo-tree"] = {
-      filesystem = {
-        filtered_items = {
-          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
-          hide_dotfiles = false,
-          hide_gitignored = true,
-        },
-      }
-    }
+    {
+      "neo-tree.nvim",
+      opts = function(config)
+        config.filesystem = {
+          filtered_items = {
+            visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+            hide_dotfiles = false,
+            hide_gitignored = true,
+          },
+        }
+        return config
+      end,
+    },
   },
 }
