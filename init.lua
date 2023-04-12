6fed2d320d6cee4dfb152a00c2418549f93deec2
romner-set <roman@skotnica.com>
2023-04-12 19:03:19 +0200

    trstuytursultrs

--- init.lua
+++ init.lua
@@ -34,30 +34,30 @@ return {
     },
 
     -- Rust
-    {
-      "simrat39/rust-tools.nvim",
-      event = "BufRead",
-      config = function()
-        local rt = require("rust-tools")
-        rt.setup({
-          server = {
-            on_attach = function(_, bufnr)
-              -- Hover actions
-              vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-              -- Code action groups
-              vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-            end,
-          },
-        })
-      end
-    },
-    {
-      'saecki/crates.nvim',
-      event = "BufRead Cargo.toml",
-      requires = { 'nvim-lua/plenary.nvim' },
-      config = function()
-        require('crates').setup()
-      end,
-    },
+    -- {
+    --   "simrat39/rust-tools.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     local rt = require("rust-tools")
+    --     rt.setup({
+    --       server = {
+    --         on_attach = function(_, bufnr)
+    --           -- Hover actions
+    --           vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+    --           -- Code action groups
+    --           vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    --         end,
+    --       },
+    --     })
+    --   end
+    -- },
+    -- {
+    --   'saecki/crates.nvim',
+    --   event = "BufRead Cargo.toml",
+    --   requires = { 'nvim-lua/plenary.nvim' },
+    --   config = function()
+    --     require('crates').setup()
+    --   end,
+    -- },
   },
 }
